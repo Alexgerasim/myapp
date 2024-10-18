044f5da1cad5e164c45ba29df7e46614e8b7a4db
alexgerasimm <alexgerasimm@gmail.com>
2024-10-18 13:29:33 +0000

    first commit

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-500">
+      <div className="text-4xl">Hello world!</div>  
+    </div>
+  );
+}
